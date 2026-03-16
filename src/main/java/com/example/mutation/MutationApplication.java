9fa428e6b1e2c532c933cd76fbcd82f156c69685
Lei TAO <Weaslypaper@163.com>
2026-03-16 12:58:02 +0800

    合并
    将子系统合并到主系统

--- src/main/java/com/example/mutation/MutationApplication.java
+++ src/main/java/com/example/mutation/MutationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MutationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MutationApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MutationApplication.class, args);
+    }
 }
+
